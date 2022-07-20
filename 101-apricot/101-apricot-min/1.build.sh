4972402bd087e1abd71fecc2cf17d9c80d6cb2f3
gnuhub <gnuhub@gmail.com>
2022-07-20 19:12:13 +0800

    build auto commit

---
+++ 101-apricot/101-apricot-min/1.build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -x
+export CMD_PATH=$(cd `dirname $0`; pwd)
+export TAG_NAME="${CMD_PATH##*/}"
+echo $TAG_NAME
+cd $CMD_PATH
+
+docker build . -t ghcr.io/linuxdeepin365/10006-docker-deepin-apricot-minimal:$TAG_NAME
+
+if [ ! -z $1 ];then
+    docker push ghcr.io/linuxdeepin365/10006-docker-deepin-apricot-minimal:$TAG_NAME
+fi
